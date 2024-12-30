0016b2d8d467fc0745b9226f26c41ac068d9dc64
meetp-lanatus <meet.p@lanatussystems.com>
2024-12-30 19:00:39 +0530

    feat: Add Ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,254 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [values, setValues] = useState({
+    income: 40000,
+    expense: 20000,
+    currentAge: 30,
+    ageToRetire: 60,
+    currentCorpus: 1000000,
+  });
+
+  const [investments, setInvestments] = useState([
+    { type: "Small Cap", returnRate: 12, value: 5000 },
+    { type: "Medium Cap", returnRate: 3, value: 8000 },
+    { type: "Large Cap", returnRate: 10, value: 12000 },
+    { type: "Bonds", returnRate: 8, value: 3000 },
+  ]);
+
+  const handleReturnChange = (index: number, value: string) => {
+    const updatedInvestments = [...investments];
+    updatedInvestments[index].returnRate = Number(value);
+    setInvestments(updatedInvestments);
+  };
+
+  const handleRangeChange = (key: string, value: number) => {
+    setValues((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const onSubmit = (data: any) => {
+    console.log("Form submitted with data:", data);
+  };
+
+  return (
+    <div className="min-h-screen bg-white text-gray-800">
+      <div className="p-6 rounded-md w-full h-full max-w-4xl mx-auto">
+        <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center text-gray-900">
+          Investment Calculator
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-6 lg:flex-row justify-between border rounded-lg shadow-lg bg-white p-6">
+            {/* Left Section */}
+            <div className="w-full lg:w-1/2">
+              {/* Monthly Income Range */}
+              <div className="my-4">
+                <div className="flex justify-between items-center mb-2">
+                  <Label htmlFor="income">Monthly Income</Label>
+                  <span className="text-sm bg-gray-200 rounded px-2 py-1 text-right w-20">
+                    {values.income}
+                  </span>
+                </div>
+                <div className="relative">
+                  <Input
+                    id="income"
+                    type="range"
+                    min={10000}
+                    max={100000}
+                    step={1000}
+                    value={values.income}
+                    {...register("income", { required: "Income is required" })}
+                    className="w-full h-2 bg-gradient-to-r from-teal-400 to-teal-600 rounded-full appearance-none px-0"
+                    onChange={(e) =>
+                      handleRangeChange("income", +e.target.value)
+                    }
+                  />
+                </div>
+              </div>
+
+              {/* Monthly Expense Range */}
+              <div className="my-4">
+                <div className="flex justify-between items-center mb-2">
+                  <Label htmlFor="expense">Monthly Expense</Label>
+                  <span className="text-sm bg-gray-200 rounded px-2 py-1 text-right w-20">
+                    {values.expense}
+                  </span>
+                </div>
+                <div className="relative">
+                  <Input
+                    id="expense"
+                    type="range"
+                    min={5000}
+                    max={50000}
+                    step={500}
+                    value={values.expense}
+                    {...register("expense", {
+                      required: "Expense is required",
+                    })}
+                    className="w-full h-2 bg-gradient-to-r from-red-400 to-orange-600 rounded-full appearance-none px-0"
+                    onChange={(e) =>
+                      handleRangeChange("expense", +e.target.value)
+                    }
+                  />
+                  {errors.expense && (
+                    <span className="text-red-500 text-sm">
+                      {errors.expense.message}
+                    </span>
+                  )}
+                </div>
+              </div>
+
+              {/* Current Age Range */}
+              <div className="my-4">
+                <div className="flex justify-between items-center mb-2">
+                  <Label htmlFor="currentAge">Current Age</Label>
+                  <span className="text-sm bg-gray-200 rounded px-2 py-1 text-right w-20">
+                    {values.currentAge}
+                  </span>
+                </div>
+                <div className="relative">
+                  <Input
+                    id="currentAge"
+                    type="range"
+                    min={18}
+                    max={80}
+                    value={values.currentAge}
+                    {...register("currentAge", {
+                      required: "Current Age is required",
+                    })}
+                    className="w-full h-2 bg-gradient-to-r from-indigo-400 to-indigo-600 rounded-full appearance-none px-0"
+                    onChange={(e) =>
+                      handleRangeChange("currentAge", +e.target.value)
+                    }
+                  />
+                </div>
+              </div>
+
+              {/* Age to Retire Range */}
+              <div className="my-4">
+                <div className="flex justify-between items-center mb-2">
+                  <Label htmlFor="ageToRetire">Age to Retire</Label>
+                  <span className="text-sm bg-gray-200 rounded px-2 py-1 text-right w-20">
+                    {values.ageToRetire}
+                  </span>
+                </div>
+                <div className="relative">
+                  <Input
+                    id="ageToRetire"
+                    type="range"
+                    min={40}
+                    max={100}
+                    value={values.ageToRetire}
+                    {...register("ageToRetire", {
+                      required: "Age to Retire is required",
+                    })}
+                    className="w-full h-2 bg-gradient-to-r from-purple-400 to-pink-600 rounded-full appearance-none px-0"
+                    onChange={(e) =>
+                      handleRangeChange("ageToRetire", +e.target.value)
+                    }
+                  />
+                </div>
+              </div>
+
+              {/* Current Investment Corpus Range */}
+              <div className="my-4">
+                <div className="flex justify-between items-center mb-2">
+                  <Label htmlFor="currentCorpus">
+                    Current Investment Corpus
+                  </Label>
+                  <span className="text-sm bg-gray-200 rounded px-2 py-1 text-right w-20">
+                    {values.currentCorpus}
+                  </span>
+                </div>
+                <div className="relative">
+                  <Input
+                    id="currentCorpus"
+                    type="range"
+                    min={0}
+                    max={10000000}
+                    step={10000}
+                    value={values.currentCorpus}
+                    {...register("currentCorpus", {
+                      required: "Current Corpus is required",
+                    })}
+                    className="w-full h-2 bg-gradient-to-r from-yellow-400 to-green-600 rounded-full appearance-none px-0"
+                    onChange={(e) =>
+                      handleRangeChange("currentCorpus", +e.target.value)
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Right Section */}
+            <div className="w-full lg:w-1/2 mt-5 lg:mt-0 lg:ml-4 border-l p-5">
+              <table className="min-w-full table-auto border-collapse">
+                <thead>
+                  <tr className="text-gray-900">
+                    <th className="py-2 px-4 border">Investment Type</th>
+                    <th className="py-2 px-4 border">Investment Value</th>
+                    <th className="py-2 px-4 border">Return (%)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {investments.map((investment, index) => (
+                    <tr key={index}>
+                      <td className="py-2 px-4 border">{investment.type}</td>
+
+                      {/* Investment Value */}
+                      <td className="py-2 px-4 border">
+                        <Input
+                          type="number"
+                          {...register(`investments[${index}].value`, {
+                            required: true,
+                          })}
+                          defaultValue={investment.value}
+                          className="w-20 px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                      </td>
+
+                      {/* Return Rate */}
+                      <td className="py-2 px-4 border">
+                        <Input
+                          type="number"
+                          {...register(`investments[${index}].returnRate`, {
+                            required: true,
+                          })}
+                          defaultValue={investment.returnRate}
+                          className="w-20 px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                          onChange={(e) => {
+                            handleReturnChange(index, e.target.value);
+                          }}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className="mt-6 flex justify-center">
+            <Button
+              type="submit"
+              className="lg:w-1/4 w-full bg-teal-600 hover:bg-teal-500 text-white rounded-lg py-2"
+            >
+              Calculate
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
